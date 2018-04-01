bf36492d8026355380c61698859c781009a6c76d
DGVaniX <37979538+DGVaniX@users.noreply.github.com>
2018-04-01 15:09:14 +0100

    en.lua
    Put this in /vrp/cfg/lang/en.lua

---
+++ sellVehToPlayer/en.lua
@@ -0,0 +1,45 @@
+vehicle = {
+    title = "Vehicle",
+    no_owned_near = "~r~No owned vehicle near.",
+    trunk = {
+      title = "Trunk",
+      description = "Open the vehicle trunk."
+    },
+    detach_trailer = {
+      title = "Detach trailer",
+      description = "Detach trailer."
+    },
+    detach_towtruck = {
+      title = "Detach tow truck",
+      description = "Detach tow truck."
+    },
+    detach_cargobob = {
+      title = "Detach cargobob",
+      description = "Detach cargobob."
+    },
+    lock = {
+      title = "Lock/unlock",
+      description = "Lock or unlock the vehicle."
+    },
+    engine = {
+      title = "Engine on/off",
+      description = "Start or stop the engine."
+    },
+    asktrunk = {
+      title = "Ask open trunk",
+      asked = "~g~Asking...",
+      request = "Do you want to open the trunk ?"
+    },
+    replace = {
+      title = "Replace vehicle",
+      description = "Replace on ground the nearest vehicle."
+    },
+    repair = {
+      title = "Repair vehicle",
+      description = "Repair the nearest vehicle."
+    },
+	sellTP = {
+		title = "Sell To Player",
+		description = "Sell the nearest vehicle to a player."
+	}
+  },
