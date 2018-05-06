5fae84837770908f425e5f28e2022e633c398f5c
DGVaniX <37979538+DGVaniX@users.noreply.github.com>
2018-05-06 20:39:33 +0100

    Fixed selling rented cars to players

--- sellVehToPlayer/basic_garage.lua
+++ sellVehToPlayer/basic_garage.lua
@@ -5,22 +5,26 @@ MySQL.createCommand("vRP/sell_vehicle_player","UPDATE vrp_user_vehicles SET user
 veh_actions[lang.vehicle.sellTP.title()] = {function(playerID,player,vtype,name)
 	if playerID ~= nil then
 		vRPclient.getNearestPlayers(player,{15},function(nplayers)
-			--vRPclient.getNearestOwnedVehicle(nplayer,{7},function(ok,vtype,name)
-				usrList = ""
-				for k,v in pairs(nplayers) do
-					usrList = usrList .. "[" .. vRP.getUserId(k) .. "]" .. GetPlayerName(k) .. " | "
-				end
-				if usrList ~= "" then
-					vRP.prompt(player,"Players Nearby: " .. usrList .. "","",function(player,user_id) 
-						user_id = user_id
-						if user_id ~= nil and user_id ~= "" then 
-							local target = vRP.getUserSource(tonumber(user_id))
-							if target ~= nil then
-								vRP.prompt(player,"Price $: ","",function(player,amount)
-									if (tonumber(amount)) then
-										MySQL.query("vRP/get_vehicle", {user_id = user_id, vehicle = name}, function(pvehicle, affected)
-											if #pvehicle > 0 then
-												vRPclient.notify(player,{"~r~The player already has this vehicle type."})
+			usrList = ""
+			for k,v in pairs(nplayers) do
+				usrList = usrList .. "[" .. vRP.getUserId(k) .. "]" .. GetPlayerName(k) .. " | "
+			end
+			if usrList ~= "" then
+				vRP.prompt(player,"Players Nearby: " .. usrList .. "","",function(player,user_id) 
+					user_id = user_id
+					if user_id ~= nil and user_id ~= "" then 
+						local target = vRP.getUserSource(tonumber(user_id))
+						if target ~= nil then
+							vRP.prompt(player,"Price $: ","",function(player,amount)
+								if (tonumber(amount)) then
+									MySQL.query("vRP/get_vehicle", {user_id = user_id, vehicle = name}, function(pvehicle, affected)
+										if #pvehicle > 0 then
+											vRPclient.notify(player,{"~r~The player already has this vehicle type."})
+										else
+											local tmpdata = vRP.getUserTmpTable(playerID)
+											if tmpdata.rent_vehicles[name] == true then
+												vRPclient.notify(player,{"~r~You cannot sell a rented vehicle!"})
+												return
 											else
 												vRP.request(target,GetPlayerName(player).." wants to sell: " ..name.. " Price: $"..amount, 10, function(target,ok)
 													if ok then
@@ -44,24 +48,24 @@ veh_actions[lang.vehicle.sellTP.title()] = {function(playerID,player,vtype,name)
 														vRPclient.notify(target,{"~r~You have refused to buy "..GetPlayerName(player).."'s car."})
 													end
 												end)
-												vRP.closeMenu(player)
-											end	
-										end) 
-									else
-										vRPclient.notify(player,{"~r~The price of the car has to be a number."})
-									end
-								end)
-							else
-								vRPclient.notify(player,{"~r~That ID seems invalid."})
-							end
+											end
+											vRP.closeMenu(player)
+										end
+									end) 
+								else
+									vRPclient.notify(player,{"~r~The price of the car has to be a number."})
+								end
+							end)
 						else
-							vRPclient.notify(player,{"~r~No player ID selected."})
+							vRPclient.notify(player,{"~r~That ID seems invalid."})
 						end
-					end)
-				else
-					vRPclient.notify(player,{"~r~No player nearby."})
-				end
-			--end)
+					else
+						vRPclient.notify(player,{"~r~No player ID selected."})
+					end
+				end)
+			else
+				vRPclient.notify(player,{"~r~No player nearby."})
+			end
 		end)
 	end
 end, lang.vehicle.sellTP.description()}
