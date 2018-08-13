5a850b35fdae0ace8fb2efe171a5f5fa34a3484d
DGVaniX <37979538+DGVaniX@users.noreply.github.com>
2018-08-13 19:26:25 +0100

    Add files via upload

---
+++ vrp_newcoin/README.txt
@@ -0,0 +1,4 @@
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+vRPnc = Proxy.getInterface("vRP_newcoin")
---
+++ vrp_newcoin/vrp_coins.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 02, 2018 at 03:09 AM
+-- Server version: 5.7.22-0ubuntu0.16.04.1
+-- PHP Version: 7.0.30-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gta13`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vrp_coins`
+--
+
+CREATE TABLE `vrp_newcoin` (
+  `user_id` int(255) NOT NULL,
+  `coins` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `vrp_newcoin`
+  ADD PRIMARY KEY (`user_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
