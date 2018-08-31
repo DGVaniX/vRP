de6689f57e3d06cc77e3e5a56115ee674e33b82d
DoubleRepo <34832750+DoubleRepo@users.noreply.github.com>
2018-08-31 04:03:52 +0200

    Update __resource.lua
    Just better this way.

--- vrp_cleanup/__resource.lua
+++ vrp_cleanup/__resource.lua
@@ -1,8 +1,18 @@
 --[[
 	SCRIPTER: DGVaniX [ DGVaniX#0096 ] 
 	WEBSITE: http://vanix.market
+	
+	Updated By 
+	
+	DoubleRepo AKA 
+	TheFlyingDutchMan AKA 
+	Mr'SuicideSheep#8623
+	https://discord.gg/a8KBSA4
 --]]
 
 client_scripts {
 	"client.lua"
+}
+server_scripts {
+	"server.lua"
 }
